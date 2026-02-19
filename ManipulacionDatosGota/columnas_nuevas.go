9186da3efacb3047dba74c41934c7e4d271b8fa4
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-02-19 17:46:45 -0500

    feat(go): implement new column creation with dataframe mutate

---
+++ ManipulacionDatosGota/columnas_nuevas.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
+)
+
+func main() {
+	f, err := os.Open("datos.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	df := dataframe.ReadCSV(f)
+
+	fmt.Println("\nDataframe original:")
+	fmt.Println(df)
+
+	var edadesDoble []int
+
+	edades := df.Col("edad").Records()
+
+	for _, e := range edades {
+		edad, err := strconv.Atoi(e)
+		if err != nil {
+			edad = 0
+		}
+		edadesDoble = append(edadesDoble, edad*2)
+	}
+
+	nuevaColumna := series.Ints(edadesDoble)
+	nuevaColumna.Name = "edad_doble"
+
+	dfNuevo := df.Mutate(nuevaColumna)
+	fmt.Println("\nDataframe con columna nueva")
+	fmt.Println(dfNuevo)
+
+}
