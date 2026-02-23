1463fb4f03b961cfd2ddcad9c75b8b41aea42ef5
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-02-23 17:45:13 -0500

    feat(stats): implement descriptive metrics and percentile calculations

---
+++ analisis-gonum/percentiles.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+
+	"github.com/go-gota/gota/dataframe"
+	"gonum.org/v1/gonum/stat"
+)
+
+func main() {
+	f1, err := os.Open("enero.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f1.Close()
+
+	dfEnero := dataframe.ReadCSV(f1)
+
+	f2, err := os.Open("febrero.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+
+	dfFebrero := dataframe.ReadCSV(f2)
+
+	df := dfEnero.RBind(dfFebrero)
+
+	ingresosStr := df.Col("ingresos").Records()
+	var ingresos []float64
+	for _, i := range ingresosStr {
+		valor, err := strconv.ParseFloat(i, 64)
+		if err != nil {
+			valor = 0.0
+		}
+		ingresos = append(ingresos, valor)
+	}
+
+	sort.Float64s(ingresos)
+
+	q1 := stat.Quantile(0.25, stat.Empirical, ingresos, nil)
+	q2 := stat.Quantile(0.50, stat.Empirical, ingresos, nil) // mediana
+	q3 := stat.Quantile(0.75, stat.Empirical, ingresos, nil)
+
+	fmt.Printf("\nPercentiles de ingresos:\n")
+	fmt.Printf("Q1 (25%%): %2f\n", q1)
+	fmt.Printf("Q2 (50%%), Mediana %2f\n", q2)
+	fmt.Printf("Q3 (75%%): %2f\n", q3)
+
+	iqr := q3 - q1
+	fmt.Printf("Rango intercuartilico (IQR): %2f\n", iqr)
+
+}
