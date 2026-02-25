b29a2391d62a333ecf14f38f407d52f3be9f8e5d
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-02-25 06:47:52 -0500

    feat: build CLI in Go with robust error handling

---
+++ automatizacion-CLI/intro_cli.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-gota/gota/dataframe"
+)
+
+func main() {
+
+	//* definir bandera
+	archivo := flag.String("archivo", "", "Ruta del archivo a procesar")
+	flag.Parse()
+
+	//* validar
+	if *archivo == "" {
+		log.Fatal("Debe proporcionar la ruta con -archivo=nombre.csv")
+	}
+
+	f, err := os.Open(*archivo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	df := dataframe.ReadCSV(f)
+	fmt.Printf("[%s] filas: %d\n", *archivo, df.Nrow())
+
+}
